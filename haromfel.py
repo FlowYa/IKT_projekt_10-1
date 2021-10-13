6f5b68fa106ca805c5679c71643d323ad819c8ad
Flow <90308447+FlowYa@users.noreply.github.com>
2021-10-13 10:36:52 +0200

    Add files via upload

---
+++ haromfel.py
@@ -0,0 +1,17 @@
+x = int(input("Hány pontszámot kaptál? "))
+
+if (x < 50):
+    print("1-es")
+
+elif (x > 50 and x < 60):
+    print("2-es")
+
+
+elif (x > 60 and x < 70):
+    print("3-as")
+
+elif (x > 70 and x < 85):
+    print("4-es")
+
+else:
+    print("5-ös")
